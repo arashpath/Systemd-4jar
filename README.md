e5fd2579e7d8103b52ccb5d203257224613b3d7b
Prashaath R <fssai.cdc@gmail.com>
2018-11-30 18:04:09 +0530

    Tested on local VM and it works ;-)

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-# Java Service Wrapper for Systemd
-
 [![Build Status](https://travis-ci.org/arashpath/Systemd-4jar.svg?branch=master)](https://travis-ci.org/arashpath/Systemd-4jar)
+# Java Service Wrapper for Systemd
+For running jar as a service
+## References
+- for [init.d](http://www.jcgonzalez.com/linux-java-service-wrapper-example)
+- for [systemd](http://www.jcgonzalez.com/ubuntu-16-java-service-wrapper-example)
+- the [one i used ;-)](https://dzone.com/articles/run-your-java-application-as-a-service-on-ubuntu)
--- systemd4jar.sh
+++ systemd4jar.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 # Creates a script in the directory of your jar file and systemd service for it.
+set -e
+
+# Java EXE location
+JAVA=$(which java)
 
 usage() {
   echo "Usage: 
@@ -10,16 +14,58 @@ usage() {
 if [ -z "$1" ]; then echo "Jar file??"; usage; exit 1; fi
 if [ ! -f "$(realpath $1)" ]; then echo "File $1 not found"; usage; exit 1; fi
 
-jar_loc="$(realpath $1)"
-jar_dir="${jar_loc%/*}"
-jar_nam="${jar_loc##*/}"
-jar_ser="${jar_nam}"
-echo "full : $jar_loc"
-echo "path : $jar_dir"
-echo "base : ${jar_loc##*/}"
-echo "ext  : ${jar_loc##*.}"
-echo "Test": ${`${jar_loc##*/}`%.*}
+# Parameter expansion http://wiki.bash-hackers.org/syntax/pe#common_use
+jar_path="$(realpath $1)"     # realpath of Jar file
+jar_dir="${jar_path%/*}"      # directory Containing Jar
+jar_name="${jar_path##*/}"    # basename with extension
+service="${jar_name%.*}"      # basename without ext and versions
 
-
+# Creating a Bash StartUp Script ----------------------------------------------#
 echo "Creating StartUp Script ..."
-#cat <<EOF > $(dirname $jar_file)/service4jar.sh
+cat <<EOF > $jar_dir/$service-start.sh
+#!/bin/bash
+# Start file for 'jar-$service.service'
+PKGS=\$(dirname \$(readlink -f "\$0") )
+JAR=\$PKGS/$jar_name
+
+# Change following is required
+JAVA_PATH=\$(dirname \$(which java))
+LOG=\$PKGS/$service.log 
+
+\$JAVA_PATH/java -jar \$JAR /tmp | tee -a \$LOG  
+EOF
+cat $jar_dir/$service-start.sh  
+
+# Creating a systemd service --------------------------------------------------#
+echo "Creating Service ..."
+cat <<EOF >/etc/systemd/system/jar-$service.service
+[Unit]
+Description=JAR Service $jar_name 
+[Service]
+ExecStart=$jar_dir/$service-start.sh
+SuccessExitStatus=143
+TimeoutStopSec=10
+Restart=on-failure
+RestartSec=5
+[Install]
+WantedBy=multi-user.target
+EOF
+cat /etc/systemd/system/jar-$service.service
+
+systemctl daemon-reload
+
+echo "
+===============================[Service Created]================================
+Created : jar-$service
+JarFile : $jar_name
+Script  : $service-start.sh
+Location: $jar_dir 
+|--------------------------------- USAGE --------------------------------------|
+to start: 
+  systemctl start jar-$service   
+Check logs:         
+  sudo journalctl -f -n 100 -u jar-$service
+Optional to enable auto startup on boot:
+  systemctl enable jar-$service
+================================================================================
+"
